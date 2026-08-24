02fc0920c264fa4dfea4a0b3a84f2d38dd60f8c7
ByeongGwang Nam <michaelis@naver.com>
2026-08-24 20:01:47 +0900

    fix: add additional CORS origins for local development

--- api/opportunities.py
+++ api/opportunities.py
@@ -23,6 +23,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "http://localhost:3001",
+        "http://127.0.0.1:3001",
     ],
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
